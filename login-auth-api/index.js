4667f3cabaf9611daaa2b709017ec2bdcbe6acbb
gilbert-kiana <gilbertintabo@gmail.com>
2022-11-04 10:37:16 +0300

    added security packages and also did the documentation with swagger.Its done

--- login-auth-api/index.js
+++ login-auth-api/index.js
@@ -2,6 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+//Swagger
+const SwaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const SwaggerDocument = YAML.load("./swagger.yaml");
+
 //middleware
 app.use(express.json());
 
@@ -18,7 +29,17 @@ const shoesRouter = require("./routes/shoes");
 
 app.use(errorHandlerMiddleware);
 
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use("/api-use", SwaggerUi.serve, SwaggerUi.setup(SwaggerDocument));
 
 //routes
 app.use("/api/v1/auth", authRouter);
